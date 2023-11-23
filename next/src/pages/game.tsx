c6393ab8923914736dbae386be8fbfe08a0d5701
Elijah M Mills <80725570+eli-mills@users.noreply.github.com>
2023-11-23 18:34:43 -0500

    Style Game and Choosing panels.

--- next/src/pages/game.tsx
+++ next/src/pages/game.tsx
@@ -1,41 +1,46 @@
-import Head from "next/head";
-import GamePanel from "@/components/GamePanel";
-import OpponentPanel from "@/components/OpponentPanel";
-import ChoosingPanel from "@/components/ChoosingPanel";
-import GameOverPanel from "@/components/GameOverPanel";
-import { GlobalContext } from "./_app";
-import { useContext } from "react";
+import Head from 'next/head'
+import GamePanel from '@/components/GamePanel'
+import OpponentPanel from '@/components/OpponentPanel'
+import ChoosingPanel from '@/components/ChoosingPanel'
+import GameOverPanel from '@/components/GameOverPanel'
+import { GlobalContext } from './_app'
+import { useContext } from 'react'
+import styles from '@/styles/Game.module.css'
 
 export default function GamePage() {
-  const { game, player } = useContext(GlobalContext);
+    const { game, player } = useContext(GlobalContext)
 
-  return (
-    <>
-      <Head>
-        <title>Wordle WS</title>
-      </Head>
-      <main>
-        {game?.status === "playing" && (
-          <>
-            <h1> {player?.name} </h1>
+    return (
+        <>
+            <Head>
+                <title>Wordle WS</title>
+            </Head>
+            <main className={styles.main}>
+                {game?.status === 'playing' && (
+                    <>
+                        <h1> {player?.name} </h1>
 
-            {game?.chooser?.socketId === player?.socketId ? (
-              <h2> You picked the word! Wait here while everyone guesses </h2>
-            ) : (
-              <GamePanel />
-            )}
-            <h2>Your Score: {player?.score}</h2>
-            <OpponentPanel />
-          </>
-        )}
-        {game?.status === "choosing" &&
-          (game?.chooser?.socketId !== player?.socketId ? (
-            <h1> {game.chooser?.name} is choosing a word </h1>
-          ) : (
-            <ChoosingPanel />
-          ))}
-        {game?.status === "end" && <GameOverPanel />}
-      </main>
-    </>
-  );
+                        {game?.chooser?.socketId === player?.socketId ? (
+                            <h2>
+                                {' '}
+                                You picked the word! Wait here while everyone
+                                guesses{' '}
+                            </h2>
+                        ) : (
+                            <GamePanel />
+                        )}
+                        <h2>Your Score: {player?.score}</h2>
+                        <OpponentPanel />
+                    </>
+                )}
+                {game?.status === 'choosing' &&
+                    (game?.chooser?.socketId !== player?.socketId ? (
+                        <h1> {game.chooser?.name} is choosing a word </h1>
+                    ) : (
+                        <ChoosingPanel />
+                    ))}
+                {game?.status === 'end' && <GameOverPanel />}
+            </main>
+        </>
+    )
 }
