d5dde5187a54185ee316bb5b82a0ff0cb421e40c
Elijah M Mills <80725570+eli-mills@users.noreply.github.com>
2023-12-05 12:19:13 -0500

    More styling of lobby page.

--- next/src/components/PlayerListPanel.tsx
+++ next/src/components/PlayerListPanel.tsx
@@ -5,30 +5,37 @@ import styles from '@/styles/PlayerListPanel.module.css'
 
 export default function PlayerListPanel() {
     const { game, player } = useContext(GlobalContext)
-
+    const playerListLength = game ? Object.keys(game.playerList).length : 0
+    const namedPlayers = game ? Object.values(game.playerList).filter(player => player.name).length : 0
     return (
         <div className={styles.playerListContainer}>
             <h2>
-                Players ({game && Object.keys(game.playerList).length} /{' '}
-                {GameParameters.MAX_PLAYERS})
+                Players ({playerListLength} / {GameParameters.MAX_PLAYERS})
             </h2>
             <ul className={styles.playerList}>
-                {game?.playerList &&
-                    Object.values(game.playerList).map((currPlayer, index) => (
-                        <li
-                            key={index}
-                            className={
-                                (currPlayer.socketId === game.leader.socketId
-                                    ? 'leader'
-                                    : '') + (currPlayer.socketId ===
-                                        player?.socketId
-                                      ? ' self'
-                                      : '')
-                            }
-                        >
-                            {currPlayer.name}
-                        </li>
-                    ))}
+                {game &&
+                    Object.values(game.playerList)
+                        .filter((currPlayer) => currPlayer.name)
+                        .map((currPlayer, index) => (
+                            <li
+                                key={index}
+                                className={
+                                    (currPlayer.socketId ===
+                                    game.leader.socketId
+                                        ? 'leader'
+                                        : '') +
+                                    (currPlayer.socketId === player?.socketId
+                                        ? ' self'
+                                        : '')
+                                }
+                            >
+                                {currPlayer.name}
+                            </li>
+                        ))}
+                {game &&
+                    Array(GameParameters.MAX_PLAYERS - namedPlayers)
+                        .fill('')
+                        .map((_, index) => <li key={index}>&nbsp;</li>)}
             </ul>
         </div>
     )
