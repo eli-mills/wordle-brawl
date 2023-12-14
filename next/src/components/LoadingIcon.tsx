15dc9f164c98520330cd0d57442b34b952a7283b
Elijah M Mills <80725570+eli-mills@users.noreply.github.com>
2023-12-14 09:17:21 -0500

    small refactor of ChoosingPanel, update some eslint rules.

--- next/src/components/LoadingIcon.tsx
+++ next/src/components/LoadingIcon.tsx
@@ -3,6 +3,7 @@ import styles from '@/styles/LoadingIcon.module.css'
 export default function LoadingIcon() {
     return (
         <div className={styles.loadingContainer}>
+            {/* eslint-disable-next-line @next/next/no-img-element*/}
             <img src="wordle-wf-loading.gif" alt="" />
             <p className={styles.loadingMessage}>
                 This app is hosted on a free-tier render.com platform and may
