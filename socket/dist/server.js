7b7f5298a2adc3be1e5e9964c6e8802e4b0a0263
Elijah M Mills <80725570+eli-mills@users.noreply.github.com>
2023-12-14 10:15:37 -0500

    remove dist folder from repo

--- socket/dist/server.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config.js';
-import { httpServer } from './socket.js';
-import { initializeDbConn } from './db.js';
-// Connect to database and start server
-initializeDbConn().then(() => {
-    httpServer.listen(process.env.PORT, () => {
-        console.log(`server listening at port ${process.env.PORT}`);
-    });
-});
